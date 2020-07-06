fddcc2348f0afbec12fe67aaf9eebc06bf83293b
noah8988 <356320520@qq.com>
2020-07-05 22:37:51 -0700

    Adding Validation and SpellChecker

--- dict.py
+++ dict.py
@@ -1,16 +1,36 @@
+import pyinputplus as pyip
+import json
+from difflib import get_close_matches
 import mysql.connector
+from spellchecker import SpellChecker
 
-con = mysql.connector.connect(
+
+# Helper Method for Input Validation
+def raiseIfNotAlpha(text):
+    if not text.isalpha():
+        raise Exception("Input must be alphabetic!")
+
+word = pyip.inputCustom(raiseIfNotAlpha, "Enter a word: ")
+
+
+
+spell = SpellChecker() # loads default word frequency list
+# TODO: Check if the input is a valid word using spellchecker
+if word not in spell:
+    # If not, suggest the top 5 possible words the user actually want to input
+    # Note: Found a bug in inputMenu() method: when a prompt message is passed to the method, the choices are not printed out.
+    # I've reported the issue on the author's github page
+    word = pyip.inputMenu(list(spell.candidates(word))[:5], numbered=True)
+    
+# Search the database for the definition
+cnx = mysql.connector.connect(
 user = "ardit700_student",
 password = "ardit700_student",
 host = "108.167.140.122",
 database = "ardit700_pm1database"
 )
 
-cursor = con.cursor()
-
-word = input("Enter a word: ")
-
+cursor = cnx.cursor()
 query = cursor.execute("SELECT * FROM Dictionary WHERE Expression = '%s'" % word)
 results = cursor.fetchall()
 
@@ -18,4 +38,7 @@ if results:
     for result in results:
         print(result[1])
 else:
-    print("No word found!")
+    print("Unfortunately, this word is not included in this dictionary!")
+
+cursor.close()
+cnx.close()
